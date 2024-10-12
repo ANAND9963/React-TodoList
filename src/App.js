60ffca28d99a69b57bd5f4bbe145fb5ad0ea0a67
Anand <yannamanenianandkumar@gmail.com>
2024-10-11 21:05:50 -0400

    First day push

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import Todo from "./components/todo"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-       
-       
-      </header>
+       <Todo />
     </div>
   );
 }
