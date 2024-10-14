819fe0853165eff512bae87142f8caa0e6742766
Anand <yannamanenianandkumar@gmail.com>
2024-10-14 17:12:34 -0400

    practice for day-2

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,36 +1,78 @@
 import "./css/style.css";
+import { useState } from "react";
 
 export default function Todo() {
-  console.log("hghgfhg");
+  const [todoList, setList] = useState(localStorage.getItem("todo")&& Array.isArray(JSON.parse(localStorage.getItem("todo"))) && JSON.parse(localStorage.getItem("todo")).length !==0 ? 
+                                    JSON.parse(localStorage.getItem("todo")):[]);
+  const [newTodo, setTodo] = useState("");
+  console.log("todoList",todoList);
+  
+  const onKeyAdd = (e) => {
+  
+    if (e.key === "Enter") {
+      btnAddTask();
+    }
+  };
+
+  const btnAddTask = () => {
+    let updatedTodoList = [...todoList, newTodo];
+    setList(updatedTodoList);
+    setStorage(JSON.stringify(updatedTodoList));
+    setTodo(""); 
+  };
+
+  const dltbtnlist= () => {
+    setList([]);
+    setStorage([]);
+  }
+  const setStorage =(array) =>{
+    localStorage.setItem("todo",array)
+  }
+
   return (
     <>
       <div className="main">
-        <div className="todo_container">
+        <div className="todo-container">
           <h2 className="header">To Do List</h2>
-          <div className="input_container">
-            <input className="txtinput" placeholder="Add a task"></input>
-            <button className="btnadd">ADD</button>
+          <div className="input-container">
+            <input
+              id="newTodoInput"
+              name="newTodo"
+              className="txtinput"
+              placeholder="Add a task"
+              value={newTodo}
+              onChange={(e) => setTodo(e.target.value)}
+              onKeyUp={onKeyAdd}
+            />
+            <button className="btnadd" onClick={btnAddTask}>
+              ADD
+            </button>
           </div>
+
           <ul className="scroll">
-            <li>Task1</li>
-            <li>swathi</li>
-            <li>swathi</li>
-            <li>saiesh</li>
-            <li>swathi</li>
-            <li>swathi</li>
-            <li>swathi</li>
-            <li>swathi</li>
-            <li>swathi</li>
+            {todoList.map((item, index) => {
+              return (
+                <div key={index} className="list-container">
+                  <input
+                    type="checkbox"
+                    name={`todoCheckbox-${index}`}
+                    id={`todoCheckbox-${index}`}
+                  />
+                  <li id={`todoLi-${index}`} key={index}>{item}</li>
+                </div>
+              );
+            })}
           </ul>
+
           <hr className="counter"></hr>
-          <div className="counter_container">
+          <div className="counter-container">
             <p>
-              Item total:<span>{1}</span>
+              Item total: <span>{todoList.length}</span>
             </p>
-            <button className="delbtn">Delete All</button>
+            <button className="delbtn" onClick={(e)=>dltbtnlist()}>Delete All</button>
           </div>
         </div>
-        <footer className="footer_txt">
+        <footer className="footer-txt">
           <span>© Developed by Anand Yannamaneni</span>
         </footer>
       </div>
